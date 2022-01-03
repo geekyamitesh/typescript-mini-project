a859b3826126540b37fdb9439b515c0f9cc75ea6
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2022-01-03 21:23:34 +0530

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+// let character = 'Amitesh';
+// let age=20;
+// let IsclassToday=true;
+// const cir = (diameter)=>{
+//   return diameter*Math.PI;
+// }
+// const circle = cir(10);
+// console.log(circle);
+// console.log(character);
+// const inputs = document.querySelectorAll('input');
+// inputs.forEach(input => {
+//   console.log(input);
+// });
+//Arrays
+var cse_student = ['amitesh', 10, 'Aditya', 20, 'Shivam', 40];
+console.log(cse_student[1]);
